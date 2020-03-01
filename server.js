117d9f2c9f0f4c9e15c2207153fb8baa679f64ba
amjudd315@gmail.com <amjudd315@gmail.com>
2020-03-01 23:43:25 +0000

    reverting

--- server.js
+++ server.js
@@ -36,8 +36,7 @@ const arcSchema = new mongoose.Schema({
   color: {
     type: String,
     default: '#bbb',
-  },
-  note: String,
+  }
 });
 
 // Create a model for arcs in the timeline.
@@ -204,5 +203,5 @@ app.put('/api/items/:id', async(req, res) => {
   }
 });
 
-let port = 4202;
+let port = 4200;
 app.listen(port, () => console.log('Server listening on port '+port));
